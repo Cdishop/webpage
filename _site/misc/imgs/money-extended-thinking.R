c2bfe556123539c816801a6c569a1bb37ec82711
Christopher Dishop <christopher@Christophers-iMac.local>
2024-06-14 10:22:33 -0400

    corrs levels

---
+++ _site/misc/imgs/money-extended-thinking.R
@@ -0,0 +1,19 @@
+pot <- 10000
+igive <- 1000*12
+theygive <- 300*12
+years <- 35
+for(i in 1:years){
+  
+  int <- pot*0.06
+  pot <- pot + int + igive + theygive
+  
+}
+pot
+
+
+
+2184 + 100 + 600 + 800
+3684*12
+44208 / 12
+
+# i need 3684 a month after taxes
